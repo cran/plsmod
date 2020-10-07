93d0766ac0171215946a69ec5352c8b9cfbe6275
Max Kuhn <max@rstudio.com>
2020-10-07 04:10:03 +0000

    version 0.1.0

--- R/pls_data.R
+++ R/pls_data.R
@@ -10,14 +10,15 @@ make_pls_mixOmics <- function() {
   parsnip::set_model_engine("pls", "classification", "mixOmics")
   parsnip::set_model_engine("pls", "regression",     "mixOmics")
   parsnip::set_dependency("pls",   "mixOmics",       "mixOmics")
+  parsnip::set_dependency("pls",   "mixOmics",       "plsmod")
 
   parsnip::set_model_arg(
     model = "pls",
     eng = "mixOmics",
-    parsnip = "num_terms",
-    original = "keepX",
-    func = list(pkg = "dials", fun = "num_terms", range = c(1, 4)),
-    has_submodel = TRUE
+    parsnip = "predictor_prop",
+    original = "predictor_prop",
+    func = list(pkg = "dials", fun = "predictor_prop"),
+    has_submodel = FALSE
   )
   parsnip::set_model_arg(
     model = "pls",
@@ -25,7 +26,7 @@ make_pls_mixOmics <- function() {
     parsnip = "num_comp",
     original = "ncomp",
     func = list(pkg = "dials", fun = "num_comp", range = c(1, 4)),
-    has_submodel = FALSE
+    has_submodel = TRUE
   )
 
   parsnip::set_fit(
@@ -36,7 +37,7 @@ make_pls_mixOmics <- function() {
       interface = "matrix",
       protect = c("x", "y"),
       func = c(pkg = "plsmod", fun = "pls_fit"),
-      defaults = list(mode = "regression")
+      defaults = list()
     )
   )
 
@@ -48,7 +49,7 @@ make_pls_mixOmics <- function() {
       interface = "matrix",
       protect = c("x", "y"),
       func = c(pkg = "plsmod", fun = "pls_fit"),
-      defaults = list(mode = "regression")
+      defaults = list()
     )
   )
 
